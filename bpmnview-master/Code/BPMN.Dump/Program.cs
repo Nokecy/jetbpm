27a2d7a3de2616d7f09a067f3a50706179c3db6d
diaskhan@yandex.kz <azrail13>
2017-09-23 21:26:30 +0600

    bpmn-view добавлен

---
+++ bpmnview-master/Code/BPMN.Dump/Program.cs
@@ -0,0 +1,82 @@
+﻿// 
+// The MIT License (MIT)
+//
+// Copyright (c) 2017 Boris Zinchenko
+// mail: info@caseagile.com
+// web: http://www.caseagile.com
+// code: https://github.com/bzinchenko/bpmnview
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BPMN.Dump
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      if (args.Length == 0)
+      {
+        System.Console.WriteLine("BPMN.Dump: Dump contenst of BPMN diagrams into text files");
+        System.Console.WriteLine("Copyright © Boris Zinchenko, 2016");
+        System.Console.WriteLine("Usege: BPMN.Dumpt [input folder] [output folder]");
+        return;
+      }
+
+      try
+      {
+        string exePath = System.Reflection.Assembly.GetEntryAssembly().Location;
+        string inputPath = args.Length > 0 ? args[0] : exePath;
+        if (!Directory.Exists(inputPath))
+        {
+          System.Console.WriteLine("Invalid input path!");
+          return;
+        }
+
+        string outputPath = args.Length > 1 ? args[1] : exePath;
+        if (!Directory.Exists(outputPath))
+        {
+          System.Console.WriteLine("Invalid output path!");
+          return;
+        }
+
+        string[] files = Directory.GetFiles(inputPath, "*.bpmn");
+        foreach (string file in files)
+        {
+          Model model = BPMN.Model.Read(file);
+
+          string outputFile = outputPath +
+            Path.GetFileNameWithoutExtension(file) + ".txt";
+
+          // todo:
+        }
+      }
+      catch (Exception ex)
+      {
+        System.Console.Write("Unhandled excepetion!");
+        System.Console.Write(ex.Message);
+      }
+    }
+  }
+}
---
+++ bpmnview-master/README.md
@@ -0,0 +1,38 @@
+# BPMN View
+A simple free tool to view and print business process diagrams in a popular BPMN format. 
+
+* Full conformance to the latest version of [BPMN 2.0 specification by OMG](http://www.bpmn.org/).
+* Import models from all [major BPM vendors](http://bpmn-miwg.github.io/bpmn-miwg-tools/)
+* Strict validation of the model according to BPMN specification.
+* Arbitrary scaling and zooming view of most complex diagrams.
+* Support of raster and vector image output.
+* Batch processing of multiple BPMN files.
+* 100% Microsoft .NET managed solution.
+* Loyal open source license for private and commercial use.
+ 
+Try it yourself!
+* Download one-click [setup for Windows](https://github.com/bzinchenko/bpmnview/blob/master/Setup/BPMNView_Setup.zip).
+* Download pre-built [binary package](https://github.com/bzinchenko/bpmnview/blob/master/Setup/BPMNView_Sources.zip).
+* Clone project repository and build it with Microsoft Visual Studio.
+* Test it on files from [offcial BPMN test suite](https://github.com/bpmn-miwg/bpmn-miwg-test-suite).
+* Build your own open source or commercial solution based on this code.
+
+ 
+![bzinchenko](Images/BPMN_View.png)
+
+##Code example
+Jump start your BPMN capable solution in minutes!
+
+Sample code to read BPMN file and save it as an image:
+
+```csharp
+Model model = BPMN.Model.Read("B.2.0.bpmn");
+Image img = model.GetImage(0, 2.0f);
+img.Save("B.2.0.png", ImageFormat.Png);
+```
+
+Below is the result:
+
+![bzinchenko](Images/B.2.0.png)
+
+BPMN View was created with support from [CaseAgile LLC](http://caseagile.com/), an innovative software and business service company specializing in integration of platforms and environments for enterprise modeling. Find more on official company page: [http://caseagile.com/](http://caseagile.com/)

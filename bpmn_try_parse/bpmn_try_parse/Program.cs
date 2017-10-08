0607f75e4448877d895d343507f2f0bad505593e
diaskhan@yandex.kz <azrail13>
2017-10-08 20:56:44 +0600

    пробуем генерить валидный bpmn файл

---
+++ bpmn_try_parse/bpmn_try_parse/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace bpmn_try_parse
+{
+    public static class Const
+    {
+        public const string bpmn2 = @"http://www.omg.org/spec/BPMN/20100524/MODEL";
+    }
+
+    #region ненжуный код
+
+    /*
+    [XmlRoot(Namespace = "http://www.url.com/foo")]
+    public class Comprobante
+    {
+        public string Tags { get; set; }
+        public Complemento Complemento { get; set; }
+    }
+
+    [XmlType(Namespace = "http://www.url.com/foo")]
+    public class Complemento
+    {
+        [XmlElement(Namespace = "http://www.url.com/child")]
+        public string Tag { get; set; }
+    }*/
+
+    #endregion
+
+    [XmlRoot(Namespace = Const.bpmn2)]
+    public class definitions
+    {
+        [XmlAttribute("id")]
+        public string id { get; set; }
+        public process process { get; set; }
+    }
+
+
+    [XmlType(Namespace = Const.bpmn2)]
+    public class process
+    {
+        [XmlElement(Namespace = Const.bpmn2)]
+        public string Tag { get; set; }
+    }
+
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+
+            /*      var child = new Complemento { Tag = "tag" };
+            var root = new Comprobante { Tags = "tags", Complemento = child };
+
+            XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+            ns.Add("rootPrefix", "http://www.url.com/foo");
+            ns.Add("childPrefix", "http://www.url.com/child");
+
+            var xs = new XmlSerializer(typeof(Comprobante));
+
+            xs.Serialize(Console.Out, root, ns);
+            */
+
+
+
+            var process1 = new process { Tag = "some unique tag" };
+            var root = new definitions { id = "id", process = process1};
+
+            XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+            ns.Add("bpmn2", Const.bpmn2);
+            //ns.Add("childPrefix", "http://www.url.com/child");
+
+            var xs = new XmlSerializer(typeof(definitions));
+
+            xs.Serialize(Console.Out, root, ns);
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ bpmn_try_parse/bpmn_try_parse/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("bpmn_try_parse")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("bpmn_try_parse")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("62c8340c-e34b-42ce-8f38-ec019a3fb8e9")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
